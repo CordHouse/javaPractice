7622d7cbad3a89d9ab70c38fd2ca47f6dd3c8213
CordHouse <qkqktl5310@gmail.com>
2022-12-18 18:41:10 +0900

    feat : (Lotto) - winningLottoNumber, bonusNumber Add(getter, setter) - 당첨된 로또 집계 함수 추가 - 로또 별로 일치하는 숫자 갯수 찾기 - 로또 별로 일치하는 보너스 찾기 (5개 일치 시 해당)

--- wtcLotto/Lotto.java
+++ wtcLotto/Lotto.java
@@ -1,11 +1,15 @@
 package wtcLotto;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Lotto {
 
     private final List<List<Integer>> lottoList = new LinkedList<>();
+    private List<Integer> winningLottoNumber = new LinkedList<>();
+    private int bonusNumber;
+    private final HashMap<String, Integer> lottoAggregation = new HashMap<>();
     public void buyLottoCycle(int cycle){
         for(int i = 0; i < cycle; i++){
             lottoList.add(new RandomNumber().getLottoNumber());
@@ -19,6 +23,55 @@ public class Lotto {
         for(List<Integer> values : lottoList){
             System.out.println(values);
         }
+    }
+
+    public void setWinningLottoNumber(List<Integer> winningLottoNumber){
+        this.winningLottoNumber = winningLottoNumber;
+    }
+
+    public void setBonusNumber(int bonusNumber){
+        this.bonusNumber = bonusNumber;
+    }
+
+    public void setLottoAggregation(){
+        for(int i = 0; i < 6; i++) {
+            String key = winningNumberMatchLottoNumber(i) + BonusNumberMatchLottoNumber(i, winningNumberMatchLottoNumber(i));
+            if(!winningNumberMatchLottoNumber(i).equals("")) {
+                lottoAggregation.put(key, lottoAggregation.getOrDefault(key, 0) + 1);
+            }
+        }
+        System.out.println(lottoAggregation);
+    }
+
+    public int getLottoAggregation(String key){
+        if(lottoAggregation.containsKey(key)){
+            return lottoAggregation.get(key);
+        }
+        return 0;
+    }
+
+    public String winningNumberMatchLottoNumber(int index){
+        int count = 0;
+        for(Integer winning : winningLottoNumber) {
+            if(getLotto(index).contains(winning)){
+                count++;
+            }
+        }
+        if(count >= 3)
+            return count+"개";
+        return "";
+    }
+
+    public String BonusNumberMatchLottoNumber(int index, String count){
+        if(!count.equals("5개")){
+            return "";
+        }
+        for(Integer winning : getLotto(index)){
+            if(winning == bonusNumber){
+                return "+보너스";
+            }
+        }
+        return "";
     }
 
 }
