1d1129a3897309943f64a141cb231a0e8135d322
CordHouse <qkqktl5310@gmail.com>
2022-12-19 03:51:51 +0900

    feat : (Input) - winningLotto 전역변수로 변경
    refactor :
    - getBuyMoney 함수 추가

--- wtcLotto/Input.java
+++ wtcLotto/Input.java
@@ -11,9 +11,13 @@ public class Input {
     private final Exception exception = new Exception();
     private int moneyMod;
     private static final int DIVISION_1000 = 1000;
+    private List<String> winningLotto;
 
     public int getMoneyMod(){
         return moneyMod / DIVISION_1000;
+    }
+    public int getBuyMoney(){
+        return moneyMod;
     }
     public int buyMoney() throws IOException {
         input = new BufferedReader(new InputStreamReader(System.in));
@@ -24,7 +28,7 @@ public class Input {
 
     public List<Integer> winningNumber() throws IOException {
         input = new BufferedReader(new InputStreamReader(System.in));
-        List<String> winningLotto = List.of(input.readLine().split(","));
+        winningLotto = List.of(input.readLine().split(","));
         exception.winningNumberError(winningLotto);
         return winningLotto.stream().map(Integer::parseInt).collect(Collectors.toList());
     }
@@ -34,6 +38,7 @@ public class Input {
         int bonus = Integer.parseInt(input.readLine());
         exception.numberError(bonus);
         exception.winningNumberRangeError(bonus);
+        exception.winningNumberAndBonusNumberSameCheck(winningLotto, bonus);
         return bonus;
     }
 }
