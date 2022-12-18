c475af5d168bc0d9196795fedee69dffcc1a2ca4
CordHouse <qkqktl5310@gmail.com>
2022-12-18 18:36:17 +0900

    feat : (Output) - Add aggregation Function

--- wtcLotto/Output.java
+++ wtcLotto/Output.java
@@ -2,6 +2,7 @@ package wtcLotto;
 
 public class Output {
     private final static String NEXT_LINE = "\n";
+    private StringBuilder sb;
     public void buyLottoMoney(){
         System.out.println("구매금액을 입력해 주세요.");
     }
@@ -16,5 +17,17 @@ public class Output {
 
     public void inputBonusNumber(){
         System.out.println(NEXT_LINE+"보너스 번호를 입력해 주세요.");
+    }
+
+    public void aggregation(Lotto lotto){
+        sb = new StringBuilder();
+        sb.append("당첨 통계").append(NEXT_LINE);
+        sb.append("---").append(NEXT_LINE);
+        sb.append("3개 일치 (5,000원) - ").append(lotto.getLottoAggregation("3개")).append("개").append(NEXT_LINE);
+        sb.append("4개 일치 (50,000원) - ").append(lotto.getLottoAggregation("4개")).append("개").append(NEXT_LINE);
+        sb.append("5개 일치 (1,500,000원) - ").append(lotto.getLottoAggregation("5개")).append("개").append(NEXT_LINE);
+        sb.append("5개 일치, 보너스 볼 일치 (30,000,000원) - ").append(lotto.getLottoAggregation("5개+보너스")).append("개").append(NEXT_LINE);
+        sb.append("6개 일치 (2,000,000,000원) - ").append(lotto.getLottoAggregation("6개")).append("개");
+        System.out.println(sb.toString());
     }
 }
