5cc0f32795e0cde6cf501466be437de08af4dfe3
CordHouse <qkqktl5310@gmail.com>
2022-12-19 03:53:46 +0900

    feat : (Main) - throws -> try catch 변경

--- wtcLotto/Main.java
+++ wtcLotto/Main.java
@@ -3,9 +3,13 @@ package wtcLotto;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        LottoGame lottoGame = new LottoGame();
-        lottoGame.init();
-        lottoGame.play();
+    public static void main(String[] args) {
+        try {
+            LottoGame lottoGame = new LottoGame();
+            lottoGame.init();
+            lottoGame.play();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
