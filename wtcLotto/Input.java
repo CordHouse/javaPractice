d7634a98a79c23284465943d779c9539cf1686e8
CordHouse <qkqktl5310@gmail.com>
2022-12-18 18:35:34 +0900

    refactor : (Input) - winningNumber return -> (void -> List<Integer>) - bonusNumber return -> (void -> int)

--- wtcLotto/Input.java
+++ wtcLotto/Input.java
@@ -4,11 +4,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Input {
     private BufferedReader input;
     private final Exception exception = new Exception();
-
     private int moneyMod;
     private static final int DIVISION_1000 = 1000;
 
@@ -22,16 +22,18 @@ public class Input {
         return moneyMod / DIVISION_1000;
     }
 
-    public void winningNumber() throws IOException {
+    public List<Integer> winningNumber() throws IOException {
         input = new BufferedReader(new InputStreamReader(System.in));
         List<String> winningLotto = List.of(input.readLine().split(","));
         exception.winningNumberError(winningLotto);
+        return winningLotto.stream().map(Integer::parseInt).collect(Collectors.toList());
     }
 
-    public void bonusNumber() throws IOException {
+    public int bonusNumber() throws IOException {
         input = new BufferedReader(new InputStreamReader(System.in));
         int bonus = Integer.parseInt(input.readLine());
         exception.numberError(bonus);
         exception.winningNumberRangeError(bonus);
+        return bonus;
     }
 }
