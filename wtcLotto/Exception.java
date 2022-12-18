e6a770d7a93f54660288f3d31f4d2ff0ca36ecda
CordHouse <qkqktl5310@gmail.com>
2022-12-19 03:50:31 +0900

    feat : (Exception) - 당첨 번호와 보너스 번호가 같은게 있을 경우 예외처리

--- wtcLotto/Exception.java
+++ wtcLotto/Exception.java
@@ -1,5 +1,6 @@
 package wtcLotto;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
@@ -8,44 +9,50 @@ public class Exception {
     private static final String number = "^[0-9]+$";
     private static final String ERROR_HEADER = "[ERROR] ";
 
-    public void numberAndModErrorCheck(int checkNumber){
+    public void numberAndModErrorCheck(int checkNumber) throws IOException {
         numberError(checkNumber);
         numberNotModError(checkNumber);
     }
-    public void numberError(int inputNumber){
+    public void numberError(int inputNumber) throws IOException {
         if(!Pattern.matches(number, String.valueOf(inputNumber))){
-            throw new IllegalArgumentException(ERROR_HEADER+"숫자만 입력해주세요.");
+            throw new IOException(ERROR_HEADER+"숫자만 입력해주세요.");
         }
     }
 
-    public void numberNotModError(int money){
+    public void numberNotModError(int money) throws IOException {
         if(money % 1000 != 0){
-            throw new IllegalArgumentException(ERROR_HEADER+"천원 단위로 입력해주세요.");
+            throw new IOException(ERROR_HEADER+"천원 단위로 입력해주세요.");
         }
     }
 
-    public void winningNumberError(List<String> winningLotto){
+    public void winningNumberError(List<String> winningLotto) throws IOException {
         lengthError(winningLotto);
         winningNumberRangeError(winningLotto);
     }
 
-    private void lengthError(List<String> winningLotto){
+    private void lengthError(List<String> winningLotto) throws IOException {
         Stream<String> winningLottoStream = winningLotto.stream();
         if(winningLottoStream.count() != 6){
-            throw new IllegalArgumentException(ERROR_HEADER+"당첨번호는 총 6자리 입니다.");
+            throw new IOException(ERROR_HEADER+"당첨번호는 총 6자리 입니다.");
         }
     }
 
-    private void winningNumberRangeError(List<String> winningLotto){
+    private void winningNumberRangeError(List<String> winningLotto) throws IOException {
         Stream<String> winningLottoStream = winningLotto.stream();
         if(winningLottoStream.anyMatch(winningNumber -> Integer.parseInt(winningNumber) < 1)){
-            throw new IllegalArgumentException(ERROR_HEADER+"1~45사이 숫자로 입력해주세요.");
+            throw new IOException(ERROR_HEADER+"1~45사이 숫자로 입력해주세요.");
         }
     }
 
-    public void winningNumberRangeError(int bonus){
+    public void winningNumberRangeError(int bonus) throws IOException {
         if(bonus < 1 || bonus > 45){
-            throw new IllegalArgumentException(ERROR_HEADER+"1~45사이 숫자로 입력해주세요.");
+            throw new IOException(ERROR_HEADER+"1~45사이 숫자로 입력해주세요.");
+        }
+    }
+
+    public void winningNumberAndBonusNumberSameCheck(List<String> winning, int bonus) throws IOException {
+        if(winning.stream().anyMatch(number -> number.equals(String.valueOf(bonus)))){
+            throw new IOException(ERROR_HEADER+"당첨번호 안에 있는 숫자입니다.");
         }
     }
 }
