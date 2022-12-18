65eb35429970213e105acea74d25b735a3de2d04
CordHouse <qkqktl5310@gmail.com>
2022-12-19 03:55:19 +0900

    feat : (WinMoney) - countOfMatch 문자형으로 변경

--- wtcLotto/WinMoney.java
+++ wtcLotto/WinMoney.java
@@ -4,35 +4,35 @@ import java.util.Arrays;
 
 public class WinMoney {
     public enum Rank {
-        FIRST(6, 2_000_000_000),
-        SECOND(5, 30_000_000),
-        THIRD(5, 1_500_000),
-        FOURTH(4, 50_000),
-        FIFTH(3, 5_000),
-        MISS(0, 0);
+        FIRST("6개", 2_000_000_000),
+        SECOND("5개+보너스", 30_000_000),
+        THIRD("5개", 1_500_000),
+        FOURTH("4개", 50_000),
+        FIFTH("3개", 5_000),
+        MISS("0개", 0);
 
-        private final int countOfMatch;
+        private final String countOfMatch;
         private final int winningMoney;
 
-        Rank(int countOfMatch, int winningMoney){
+        Rank(String countOfMatch, int winningMoney){
             this.countOfMatch = countOfMatch;
             this.winningMoney = winningMoney;
         }
 
-        public static Rank findByRank(int code){
+        public static Rank findByRank(String keyCode){
             return Arrays.stream(Rank.values())
-                    .filter(rank -> rank.countOfMatch == code)
+                    .filter(rank -> rank.countOfMatch.equals(keyCode))
                     .findAny()
                     .orElse(MISS);
         }
 
-        public int getCountOfMatch() {
+        public String getCountOfMatch() {
             return countOfMatch;
         }
 
-        public int getWinningMoney(long countOfMatch) {
+        public int getWinningMoney(String countOfMatch) {
             for(Rank rank : Rank.values()){
-                if(countOfMatch == rank.getCountOfMatch()){
+                if(countOfMatch.equals(rank.getCountOfMatch())){
                     return winningMoney;
                 }
             }
