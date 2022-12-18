5d130d730a60841722f4a1754da13433836e70d2
CordHouse <qkqktl5310@gmail.com>
2022-12-18 18:37:03 +0900

    refactor : (Main) - init()과 play()함수를 구분하여 실행

--- wtcLotto/Main.java
+++ wtcLotto/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         LottoGame lottoGame = new LottoGame();
+        lottoGame.init();
         lottoGame.play();
     }
 }
