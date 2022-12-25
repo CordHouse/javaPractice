55e5dc1417f54a49259978c1ed633af0bceeff0a
CordHouse <qkqktl5310@gmail.com>
2022-12-26 01:26:09 +0900

    feat : (Output) - 빈줄 만드는 함수 생성 - 재시작 확인하는 함수 생성
    refactor :
    - up, down 코드 수정

--- wtcBridge/Output.java
+++ wtcBridge/Output.java
@@ -2,13 +2,21 @@ package wtcBridge;
 
 public class Output {
     private final String NEXT_LINE = "\n";
+    private String up;
+    private String down;
     public void printMap(String up, String down){
+        this.up = up;
+        this.down = down;
         System.out.println(up);
         System.out.println(down);
     }
 
-    public void printResult(){
-
+    public void printResult(String result, int count){
+        System.out.println("최종 게임 결과");
+        System.out.println(up);
+        System.out.println(down);
+        System.out.println("게임 성공 여부: " + result);
+        System.out.println("총 시도한 횟수: " + count);
     }
 
     public void printStartMessage(){
@@ -21,5 +29,13 @@ public class Output {
 
     public void printMoveMessage(){
         System.out.println("이동할 칸을 선택해주세요.");
+    }
+
+    public void printBlankLine(){
+        System.out.println();
+    }
+
+    public void printAskRetry(){
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
     }
 }
