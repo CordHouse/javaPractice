703726e778d136327d1fdb29525417e48d8cb4c7
CordHouse <qkqktl5310@gmail.com>
2022-12-26 01:23:52 +0900

    feat : (BridgeGame) Add - 초기 벨류 설정 - 게임 종료 표시 - 실패 시 이동표시 - 반대편 다리 빈칸으로 표시
    Refactor :
    - 다리 이동 시 출력문 코드 수정
    - 초기 세팅 함수 코드 수정

--- wtcBridge/BridgeGame.java
+++ wtcBridge/BridgeGame.java
@@ -8,70 +8,133 @@ public class BridgeGame {
     private Input input = new Input();
     private MakeBridge makeBridge = new MakeBridge();
     private List<String> madeBridge;
-    private boolean MOVE_CHECK = true;
-    private int index = 0;
+    private int bridgeSize;
+    private boolean MOVE_CHECK;
+    private int index;
+    private int retryCount = 1;
     private String currentMove;
-    private String up = "";
-    private String down = "";
+    private String up;
+    private String down;
+    private final String SUCCESS = "성공";
+    private final String FAIL = "실패";
+
     public void init() throws IOException {
         output.printStartMessage();
         output.printInputBridgeLength();
-        madeBridge = makeBridge.makeBridge(input.readBridgeSize());
+        bridgeSize = input.readBridgeSize();
+        madeBridge = makeBridge.makeBridge(bridgeSize);
+        output.printBlankLine();
+        valueInit();
     }
 
-    public void start(){
-        do{
+    public void valueInit(){
+        MOVE_CHECK = true;
+        index = 0;
+        up = "";
+        down = "";
+    }
+
+    public void start() throws IOException {
+        while (MOVE_CHECK && bridgeSize > index) {
             move();
             check(index, currentMove);
+            output.printMap(up, down);
             index++;
-        } while(MOVE_CHECK);
-        retry();
+        }
+        if(bridgeSize != index)
+            retry();
     }
 
-    public void move(){
+    public void move() throws IOException {
         output.printMoveMessage();
         currentMove = input.readMoving();
     }
 
-    public void retry(){
-
+    public void retry() throws IOException {
+        output.printAskRetry();
+        if(input.readGameCommand().equals("R")){
+            valueInit();
+            retryCount++;
+            start();
+        }
     }
 
-    public void check(int index, String move){
-        if(!madeBridge.get(index).equals(move))
+    public void gameOver(){
+        if(bridgeSize == index) {
+            output.printResult(SUCCESS, retryCount);
+            return;
+        }
+        output.printResult(FAIL, retryCount);
+    }
+
+    public void check(int index, String move) {
+        if (!madeBridge.get(index).equals(move)) {
             MOVE_CHECK = false;
-    }
-
-    public void successMove(){
-        if(currentMove.equals("U")){
+            failMove();
+            return;
         }
+        successMove();
     }
 
-    public String successUp(){
-        if(up.isEmpty()){
+    public void successMove() {
+        if (currentMove.equals("U")) {
+            up = successUp();
+            down = blankDown();
+            return;
+        }
+        down = successDown();
+        up = blankUp();
+    }
+
+    public void failMove() {
+        if (currentMove.equals("U")) {
+            up = failUp();
+            down = blankDown();
+            return;
+        }
+        down = failDown();
+        up = blankUp();
+    }
+
+    public String successUp() {
+        if (up.isEmpty()) {
             return "[ O ]";
         }
-        return up.substring(up.length()-1)+"| O ]";
+        return up.substring(0, up.length() - 1) + "| O ]";
     }
 
-    public String failUp(){
-        if(up.isEmpty()){
+    public String failUp() {
+        if (up.isEmpty()) {
             return "[ X ]";
         }
-        return up.substring(up.length()-1)+"| X ]";
+        return up.substring(0, up.length() - 1) + "| X ]";
     }
 
-    public String successDown(){
-        if(down.isEmpty()){
+    public String successDown() {
+        if (down.isEmpty()) {
             return "[ O ]";
         }
-        return down.substring(down.length()-1)+"| O ]";
+        return down.substring(0, down.length() - 1) + "| O ]";
     }
 
-    public String failDown(){
-        if(down.isEmpty()){
+    public String failDown() {
+        if (down.isEmpty()) {
             return "[ X ]";
         }
-        return down.substring(down.length()-1)+"| X ]";
+        return down.substring(0, down.length() - 1) + "| X ]";
+    }
+
+    public String blankUp() {
+        if (up.isEmpty()) {
+            return "[   ]";
+        }
+        return up.substring(0, up.length() - 1) + "|   ]";
+    }
+
+    public String blankDown() {
+        if (down.isEmpty()) {
+            return "[   ]";
+        }
+        return down.substring(0, down.length() - 1) + "|   ]";
     }
 }
